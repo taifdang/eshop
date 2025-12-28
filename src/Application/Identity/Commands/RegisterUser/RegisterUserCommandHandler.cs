e9b2073755e9e12e5973cdd249354e570ecc5d14
LAPTOP-J20BGGNG\taida <taidangduc1503@gmail.com>
2025-12-28 13:44:44 +0700

    fix: update event create customer when new user

--- src/Application/Identity/Commands/RegisterUser/RegisterUserCommandHandler.cs
+++ src/Application/Identity/Commands/RegisterUser/RegisterUserCommandHandler.cs
@@ -1,5 +1,7 @@
 ﻿using Application.Common.Interfaces;
 using Application.Common.Models;
+using Contracts.IntegrationEvents;
+using EventBus.Abstractions;
 using MediatR;
 
 namespace Application.Identity.Commands.RegisterUser;
@@ -7,15 +9,23 @@ namespace Application.Identity.Commands.RegisterUser;
 public class RegisterUserCommandHandler : IRequestHandler<RegisterUserCommand, RegisterUserResult>
 {
     private readonly IIdentityService _identityService;
+    private readonly IEventPublisher _eventPublisher;
 
-    public RegisterUserCommandHandler(IIdentityService identityService)
+    public RegisterUserCommandHandler(IIdentityService identityService, IEventPublisher eventPublisher)
     {
         _identityService = identityService;
+        _eventPublisher = eventPublisher;
     }
 
     public async Task<RegisterUserResult> Handle(RegisterUserCommand command, CancellationToken cancellationToken)
     {
         var result = await _identityService.Register(new RegisterUserRequest(command.UserName, command.Email, command.Password));
+
+        await _eventPublisher.PublishAsync(new UserCreatedIntegrationEvent
+        {
+            UserId = result.Id,
+            Email = result.Email,
+        });
 
         return result;
     }
