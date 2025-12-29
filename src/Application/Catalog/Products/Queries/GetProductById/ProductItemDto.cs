5301365722f632d9870161c8f3c84b570e072ef4
LAPTOP-J20BGGNG\taida <taidangduc1503@gmail.com>
2025-12-29 22:22:40 +0700

    chore: update dto

--- src/Application/Catalog/Products/Queries/GetProductById/ProductItemDto.cs
+++ src/Application/Catalog/Products/Queries/GetProductById/ProductItemDto.cs
@@ -6,8 +6,8 @@ public class ProductItemDto
 {
     public Guid Id { get; init; }
     public string Title { get; init; }
-    public decimal MinPrice { get; init; }
-    public decimal MaxPrice { get; init; }
+    //public decimal MinPrice { get; init; }
+    //public decimal MaxPrice { get; init; }
     public string Description { get; init; }
     public string Category { get; init; }
     public VariantBriefDto? VariantBrief { get; set; }
